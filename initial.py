112258eaab5b470a41c9ddcfff8852baf19749b5
andrianipanagi <andriani.panagi2608@hotmail.com>
2023-05-15 13:48:24 +0300

    some changes

--- initial.py
+++ initial.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 
 # Creating initial dataframe 
-df = pd.DataFrame(columns=['product_name','product_price','date_time_scraped','product_subclass','retailer','subclass_average'])
+df = pd.DataFrame(columns=['product_name','product_price','date_time_scraped','product_subclass','retailer'])
 
 # Create csv file to store our date
 df.to_csv("BillionPricesProject_ProductList.csv",index=False)
