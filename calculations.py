4ddbd350975d759ffc1d1b040d5d0869c0e23077
andrianipanagi <andriani.panagi2608@hotmail.com>
2024-02-29 13:27:20 +0200

    code for last thursday corrected

--- calculations.py
+++ calculations.py
@@ -82,29 +82,31 @@ thursdays = sorted(thursdays)
 #calculations['CPI_general_lastthursday'] = None
 #calculations['monthly_inflation_lastthursday'] = None
 
-# for i in range(len(thursdays)):
-#     if calculations[calculations['date']==thursdays[i]].empty:
-#         try:
-#             rate = calculations[(calculations['subclass']=='rice')&(calculations['date']==thursdays[i]-timedelta(days=1))].iloc[0]['CPI_general']
-#             calculations.loc[(calculations['date']==thursdays[i]-timedelta(days=1)),'CPI_general_lastthursday'] = rate
-#             if thursdays[i-1]-timedelta(days=1)<thursdays[i]-timedelta(days=1) and not calculations.loc[calculations['date']==thursdays[i]-timedelta(days=1),'CPI_general_lastthursday'].empty:
-#                 prev_rate = calculations[(calculations['subclass']=='rice')&(calculations['date']==thursdays[i-1]-timedelta(days=1))].iloc[0]['CPI_general']
-#                 calculations.loc[(calculations['date']==thursdays[i]-timedelta(days=1)),'monthly_inflation_lastthursday'] = round(100*(rate - prev_rate)/rate,4)
-#             else:
-#                 calculations.loc[(calculations['date']==thursdays[i]),'monthly_inflation_lastthursday'] = None
+for i in range(len(thursdays)):
+    if calculations[calculations['date']==thursdays[i]].empty:
+        try:
+            rate = calculations[(calculations['subclass']=='rice')&(calculations['date']==thursdays[i]-timedelta(days=1))].iloc[0]['CPI_general']
+            calculations.loc[(calculations['date']==thursdays[i]-timedelta(days=1)),'CPI_general_lastthursday'] = rate
+            if thursdays[i-1]-timedelta(days=1)<thursdays[i]-timedelta(days=1) and not calculations.loc[calculations['date']==thursdays[i],'CPI_general_lastthursday'].empty:
+                prev_rate = calculations[(calculations['subclass']=='rice')&(calculations['date']==thursdays[i-1]-timedelta(days=1))].iloc[0]['CPI_general']
+                calculations.loc[(calculations['date']==thursdays[i]-timedelta(days=1)),'monthly_inflation_lastthursday'] = round(100*(rate - prev_rate)/rate,4)
+            else:
+                calculations.loc[(calculations['date']==thursdays[i]),'monthly_inflation_lastthursday'] = None
              
-#         except IndexError:
-#             calculations.loc[(calculations['date']==thursdays[i]),'CPI_general_lastthursday'] = None
-        
+        except IndexError:
+            calculations.loc[(calculations['date']==thursdays[i]),'CPI_general_lastthursday'] = None
             
-#     else:
-#         rate = calculations[(calculations['subclass']=='rice')&(calculations['date']==thursdays[i])].iloc[0]['CPI_general']
-#         calculations.loc[(calculations['date']==thursdays[i]),'CPI_general_lastthursday'] = rate
-#         if thursdays[i-1]<thursdays[i] and not calculations.loc[calculations['date']==thursdays[i],'CPI_general_lastthursday'].empty:
-#             prev_rate = calculations[(calculations['subclass']=='rice')&(calculations['date']==thursdays[i-1])].iloc[0]['CPI_general']
-#             calculations.loc[(calculations['date']==thursdays[i]),'monthly_inflation_lastthursday'] = round(100*(rate - prev_rate)/rate,4)
-#         else:
-#             calculations.loc[(calculations['date']==thursdays[i]),'monthly_inflation_lastthursday'] = None
+    else:
+        rate = calculations[(calculations['subclass']=='rice')&(calculations['date']==thursdays[i])].iloc[0]['CPI_general']
+        calculations.loc[(calculations['date']==thursdays[i]),'CPI_general_lastthursday'] = rate
+        if thursdays[i-1]<thursdays[i] and not calculations.loc[calculations['date']==thursdays[i],'CPI_general_lastthursday'].empty:
+            try:
+                prev_rate = calculations[(calculations['subclass']=='rice')&(calculations['date']==thursdays[i-1])].iloc[0]['CPI_general']
+            except IndexError:
+                prev_rate = calculations[(calculations['subclass']=='rice')&(calculations['date']==thursdays[i-1]-timedelta(days=1))].iloc[0]['CPI_general']
+            calculations.loc[(calculations['date']==thursdays[i]),'monthly_inflation_lastthursday'] = round(100*(rate - prev_rate)/rate,4)
+        else:
+            calculations.loc[(calculations['date']==thursdays[i]),'monthly_inflation_lastthursday'] = None
         
 
 
