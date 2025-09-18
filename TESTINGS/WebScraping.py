70d1429aaa5cb3554760913e8a4533a71933ded6
kvitalis <157591960+kvitalis@users.noreply.github.com>
2025-09-18 11:54:26 +0300

    Update WebScraping.py

--- TESTINGS/WebScraping.py
+++ TESTINGS/WebScraping.py
@@ -2495,7 +2495,7 @@ def results_intercity_buses(u):
     #response = requests.get(url_new)
     ## 2
     bs = BeautifulSoup(url_new, "html.parser")
-    response = requests.get(bs, {'headers':header})
+    response = requests.get(bs)
     
     ### with headers (*ERROR*)
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
