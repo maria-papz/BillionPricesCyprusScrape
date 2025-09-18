ae082ab4a9f61c28055686bd2619dc2cc1517add
kvitalis <157591960+kvitalis@users.noreply.github.com>
2025-09-18 12:49:24 +0300

    Update WebScraping.py

--- TESTINGS/WebScraping.py
+++ TESTINGS/WebScraping.py
@@ -3092,15 +3092,15 @@ def results_public(u):
     #bs = BeautifulSoup(Item_url_, "html.parser")
     #response = requests.get(bs)
     ## 2 
-    #response = requests.get(Item_url_)
+    response = requests.get(Item_url_)
     
     ### with headers 
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
     #header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
     #header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
     ## 1 
-    bs = BeautifulSoup(Item_url_, "html.parser")
-    response = requests.get(bs, {'headers':header})
+    #bs = BeautifulSoup(Item_url_, "html.parser")
+    #response = requests.get(bs, {'headers':header})
     ## 2 
     #response = requests.get(Item_url_, headers = header) 
 
