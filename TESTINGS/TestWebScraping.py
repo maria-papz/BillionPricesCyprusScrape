bc23d2885b54b8284062af0b96441774fd1bb281
kvitalis <157591960+kvitalis@users.noreply.github.com>
2026-03-04 13:08:08 +0200

    Update TestWebScraping.py

--- TESTINGS/TestWebScraping.py
+++ TESTINGS/TestWebScraping.py
@@ -29,6 +29,20 @@ from docx import Document
 ##### Kyriaco's testings
 
 #================================================================================================================================
+# Alphamega
+#================================================================================================================================
+print("Alphamega")
+url = "https://www.alphamega.com.cy/en/groceries/personal-care/mens-toiletries/shaving/gillette-blue-ii-plus-slalom-disposable-razors-5-pieces"
+
+response_1 = requests.get(url)
+print(response_1)
+
+header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',}
+#header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
+response_2 = requests.get(url, headers=header)
+print(response_2)
+
+#================================================================================================================================
 # SupermarketCy
 #================================================================================================================================
 print("SupermarketCy")
