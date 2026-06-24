df464e679dc03bdf41642322c195fc5e7b599448
kvitalis <157591960+kvitalis@users.noreply.github.com>
2026-06-24 10:55:24 +0300

    Update IterationB.py

--- ECOICOPv2/IterationB.py
+++ ECOICOPv2/IterationB.py
@@ -626,10 +626,10 @@ def results_electroline(u):
         soup = BeautifulSoup(response.content, "html.parser")
         element_soup = soup.find_all("ins", {"class":"product-price product-price--single product-price--sale-price product-price--single--sale-price"}) 
         if element_soup:
-            price_ = element_soup[0].text.replace("\n",'').replace("€","")
+            price_ = element_soup[0].text.replace("\n",'').replace("€","").replace(" ","")
         else:
             element_soup = soup.find_all("h2", {"class":"product-price product-price--single"}) 
-            price_ = element_soup[0].text.replace("\n","").replace("€","")
+            price_ = element_soup[0].text.replace("\n","").replace("€","").replace(" ","")
         print(price_)
         new_row.append(datetime.now().strftime('%Y-%m-%d'))
         new_row.append(name_)
