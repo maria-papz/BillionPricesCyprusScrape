d006f4af64265d4bff8c36b86cdc57a51cbe725d
kvitalis <157591960+kvitalis@users.noreply.github.com>
2026-06-25 13:10:28 +0300

    Update TestWebScraping.py

--- TESTINGS/TestWebScraping.py
+++ TESTINGS/TestWebScraping.py
@@ -29,20 +29,6 @@ from docx import Document
 ##### Kyriacos' testings
 
 #================================================================================================================================
-# Alphamega
-#================================================================================================================================
-print("Alphamega")
-url = "https://www.alphamega.com.cy/en/groceries/personal-care/mens-toiletries/shaving/gillette-blue-ii-plus-slalom-disposable-razors-5-pieces"
-
-response_1 = requests.get(url)
-print(response_1)
-
-#header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',}
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
-response_2 = requests.get(url, headers=header)
-print(response_2)
-
-#================================================================================================================================
 # SupermarketCy
 #================================================================================================================================
 print("SupermarketCy")
@@ -169,20 +155,6 @@ response_2 = requests.get(url, headers=header)
 print(response_2)
 
 #================================================================================================================================
-# Evdokia Jewellery
-#================================================================================================================================
-print("Evdokia Jewellery")
-url = "https://evdokiajewellery.com/product/petite-sheffield"
-
-response_1 = requests.get(url)
-print(response_1)
-
-#header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',}
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
-response_2 = requests.get(url, headers=header)
-print(response_2)
-
-#================================================================================================================================
 # Famous Sports
 #================================================================================================================================
 print("Famous Sports")
@@ -280,31 +252,6 @@ response_2 = requests.get(url, headers=header)
 print(response_2)
 
 #================================================================================================================================
-# EOA Nicosia (https://ndlgo.org.cy/)
-#================================================================================================================================
-print("Water Board of Nicosia")
-url = "https://ndlgo.org.cy/water-supply/water-fees-wbn/"
-
-response_1 = requests.get(url)
-print(response_1)
-
-#header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',}
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
-response_2 = requests.get(url, headers=header)
-print(response_2)
-
-print("Sewerage Board of Nicosia")
-url = "https://ndlgo.org.cy/sewage/sewer-fees/"
-
-response_1 = requests.get(url)
-print(response_1)
-
-#header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',}
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
-response_2 = requests.get(url, headers=header)
-print(response_2)
-
-#================================================================================================================================
 # EOA Limassol (https://eoalemesos.org.cy/el/fees)
 #================================================================================================================================
 print("Water/Sewerage Board of Limassol")
@@ -321,10 +268,21 @@ response_2B = requests.get(url, headers=headerB)
 print(response_2B)
 
 #================================================================================================================================
-# Music Avenue
+# EOA Nicosia (https://ndlgo.org.cy/)
 #================================================================================================================================
-print("Music Avenue")
-url = "https://www.musicavenue.com.cy/product/stagg-c505-1-4-2/"
+print("Water Board of Nicosia")
+url = "https://ndlgo.org.cy/water-supply/water-fees-wbn/"
+
+response_1 = requests.get(url)
+print(response_1)
+
+#header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',}
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
+response_2 = requests.get(url, headers=header)
+print(response_2)
+
+print("Sewerage Board of Nicosia")
+url = "https://ndlgo.org.cy/sewage/sewer-fees/"
 
 response_1 = requests.get(url)
 print(response_1)
@@ -376,37 +334,9 @@ header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 response_2 = requests.get(url, headers=header)
 print(response_2)
 
-#================================================================================================================================
-# Numbeo
-#================================================================================================================================
-print("Numbeo")
-url = "https://www.numbeo.com/cost-of-living/country_price_rankings?itemId=17&displayCurrency=EUR"
-
-response_1 = requests.get(url)
-print(response_1)
-
-#header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',}
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
-response_2 = requests.get(url, headers=header)
-print(response_2)
-
-#================================================================================================================================
-# Max 7 Taxi
-#================================================================================================================================
-print("Max 7 Taxi")
-url = "https://www.max7taxi.com/?mod=info&show=excursions"
-
-response_1 = requests.get(url)
-print(response_1)
-
-#header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',}
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
-response_2 = requests.get(url, headers=header)
-print(response_2)
-
 #################################################################################################################################
 print("########################################################################################################################")
-print("ECOICOP v2")
+print("#######################################              ECOICOP v2          ###############################################")
 print("########################################################################################################################")
 #################################################################################################################################
 
