3c5b8ae30920fc36a172308a69b7c608f21daf3e
kvitalis <157591960+kvitalis@users.noreply.github.com>
2026-06-29 21:08:00 +0300

    Update Calculations-2.py

--- ECOICOPv2/PythonCodes/Calculations-2.py
+++ ECOICOPv2/PythonCodes/Calculations-2.py
@@ -11,8 +11,8 @@ from datetime import datetime, timedelta
 warnings.simplefilter("ignore")
 
 # Date
-today = datetime.today().strftime("%Y-%m-%d")
-#today = '2026-06-25'
+#today = datetime.today().strftime("%Y-%m-%d")
+today = '2026-06-29'
 
 # Read necessary data 
 #raw_data_26q3 = pd.read_csv("ECOICOPv2/Datasets/Raw-Data/Raw-Data-2-2026Q3.csv", parse_dates = ['Date'], date_parser = lambda x:pd.to_datetime(x, format = '%Y-%m-%d'))
@@ -195,10 +195,10 @@ current_df = df_daily_cpi_division[df_daily_cpi_division["Date"] == current_day_
 unique_divisions = current_df['Division'].unique()
 
 for unique_ in unique_divisions:
-    df_13 = float(prior_df[prior_df["Division"] == unique_]["CPI Division"])
-    df_14 = float(current_df[current_df["Division"] == unique_]["CPI Division"])
-    #df_13 = prior_df[prior_df["Division"] == unique_]["CPI Division"]
-    #df_14 = current_df[current_df["Division"] == unique_]["CPI Division"]
+    #df_13 = float(prior_df[prior_df["Division"] == unique_]["CPI Division"])
+    #df_14 = float(current_df[current_df["Division"] == unique_]["CPI Division"])
+    df_13 = prior_df[prior_df["Division"] == unique_]["CPI Division"]
+    df_14 = current_df[current_df["Division"] == unique_]["CPI Division"]
     percentage_change = 100 * (df_14 - df_13) / df_13
     
     index_list = current_df[current_df["Division"] == unique_]["CPI Division"].index.tolist()
@@ -214,10 +214,10 @@ current_df = df_daily_cpi_subclass_division[df_daily_cpi_subclass_division["Date
 unique_divisions = current_df['Subclass'].unique()
 
 for unique_ in unique_divisions:
-    df_15 = float(prior_df[prior_df["Subclass"] == unique_]["CPI Division"])
-    df_16 = float(current_df[current_df["Subclass"] == unique_]["CPI Division"])
-    #df_15 = prior_df[prior_df["Subclass"] == unique_]["CPI Division"]
-    #df_16 = current_df[current_df["Subclass"] == unique_]["CPI Division"]
+    #df_15 = float(prior_df[prior_df["Subclass"] == unique_]["CPI Division"])
+    #df_16 = float(current_df[current_df["Subclass"] == unique_]["CPI Division"])
+    df_15 = prior_df[prior_df["Subclass"] == unique_]["CPI Division"]
+    df_16 = current_df[current_df["Subclass"] == unique_]["CPI Division"]
     percentage_change = 100 * (df_16 - df_15) / df_15            
     
     index_list = current_df[current_df["Subclass"] == unique_]["CPI Division"].index.tolist()
