28294d570bd74c7d00b29ad3185f29f4fdc39aef
kvitalis <157591960+kvitalis@users.noreply.github.com>
2026-08-03 13:22:56 +0300

    Update TestWebScraping.py

--- TESTINGS/TestWebScraping.py
+++ TESTINGS/TestWebScraping.py
@@ -255,6 +255,21 @@ html_content = fetch_url(url, headers)
 if html_content:
     print(html_content[:500])
 '''
+#================================================================================================================================
+# Larnaca Driving School
+#================================================================================================================================
+print("Larnaca Driving School")
+url = "https://larnacadrivingschool.com/el/"
+
+response_1 = requests.get(url)
+print(response_1)
+
+#header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',}
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
+response_2 = requests.get(url, headers=header)
+print(response_2)
+
+
 
 """
 #####################################################################################################################################################
